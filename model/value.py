d472ed69758d995f79466e3c2032c298dc0cf254
danielgstephenson <dgstephenson.econ@gmail.com>
2025-12-25 22:59:22 -0500

    advance

--- model/value.py
+++ model/value.py
@@ -10,7 +10,9 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("device = " + str(device))
 torch.set_printoptions(sci_mode=False)
 
+advance = True
 model = ValueModel().to(device)
+target_model = ValueModel().to(device).eval() if advance else get_reward
 optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
 checkpoint_path = f'./checkpoints/value_checkpoint.pt'
 if os.path.exists(checkpoint_path):
@@ -18,19 +20,17 @@ if os.path.exists(checkpoint_path):
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-lr = 0.00001
-for param_group in optimizer.param_groups:
-    param_group['lr'] = lr
-
-advance = False
-target_model = ValueModel().to(device).eval() if advance else get_reward
 if isinstance(target_model, ValueModel):
     target_model.load_state_dict(model.state_dict())
+
+lr = 0.000001
+for param_group in optimizer.param_groups:
+    param_group['lr'] = lr
 
 print('Saving onnx...')
 save_onnx(model, './onnx/value.onnx', device)
 
-batch_size = 30000
+batch_size = 3000
 generator = Generator(batch_size, device)
 
 # quit()
@@ -39,7 +39,7 @@ discount = 0.9
 self_noise = 0.5
 other_noise = 0
 smooth_loss = 0
-loss_smoothing = 0.05
+loss_smoothing = 0.01
 target_smoothing = 0.001
 print('Training...')
 for batch in range(1000000000000):
@@ -67,11 +67,11 @@ for batch in range(1000000000000):
     optimizer.step()
     save_checkpoint(model, optimizer, checkpoint_path)
     delta = torch.tensor(0.0).to(device)
-    if isinstance(target_model, ValueModel):
+    if isinstance(target_model, ValueModel) and batch > 100 and smooth_loss < 1:
         with torch.no_grad():
             for target_param, param in zip(target_model.parameters(), model.parameters()):
-                delta += torch.sum((param.data-target_param.data)**2)
                 new_target_param = target_smoothing * param.data + (1.0 - target_smoothing) * target_param.data
+                delta += torch.sum((new_target_param-target_param.data)**2)
                 target_param.data.copy_(new_target_param)
-    delta = torch.sqrt(delta)
-    print(f'Batch: {batch}, LR: {lr:.8f}, Loss: {loss_value:07.4f}, Smooth: {smooth_loss:07.4f}, Delta: {delta:05.2f}')
+    delta = 10000*torch.sqrt(delta)
+    print(f'Batch: {batch}, LR: {lr:.8f}, Loss: {loss_value:07.4f}, Smooth: {smooth_loss:07.4f}, Delta: {delta:010.08f}')
